544bde5828f0ebff635158087a4b68eb568be2a6
Niels Thole <niels.thole@gmail.com>
2017-11-08 09:27:49 +0100

    Initial commit (currently working)

---
+++ BrokenModule/module.php
@@ -0,0 +1,25 @@
+<?
+class TestTest extends IPSModule {
+    
+    public function Create(){
+        //Never delete this line!
+        parent::Create();
+    }
+
+    public function Destroy(){
+        //Never delete this line!
+        parent::Destroy();
+        
+    }
+
+    public function ApplyChanges(){
+        //Never delete this line!
+        parent::ApplyChanges();
+    }
+    
+    public function DoSomething() {
+        
+    }
+}
+
+?>
